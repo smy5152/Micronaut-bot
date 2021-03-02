6560879b54bd34a9237ac8a09975a17265d684ae
smy5152 <shabbiryamani@gmail.com>
2021-03-03 00:00:38 +0200

    FIX: Review Changes & Feedback ADD: Modules to all files RSPEC: Tests added for +ve & -ve scenario RUBOCOP: Restored original File.

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,16 @@
 require_relative '../lib/Messages'
 require_relative '../lib/Bot'
 
-(1..3).each { puts '.'; sleep 0.5 }
-puts 'WOOHOO!!! BOT is Running... - Catch it with commands on Telegram here... http://t.me/smy5152_bot'
-(1..5).each { puts '.'; sleep 0.5 }
+module MicronautBot
+  (1..3).each do
+    puts '.'
+    sleep 0.5
+  end
+  puts 'WOOHOO!!! BOT is Running... - Catch it with commands on Telegram here... http://t.me/smy5152_bot'
+  (1..5).each do
+    puts '.'
+    sleep 0.5
+  end
 
-MicronautBot.new
+  Bot.new
+end
--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -1,34 +1,43 @@
 # spec/messages_spec.rb
-require_relative '../lib/messages'
+require_relative '../lib/Messages'
+module MicronautBot
+  describe 'Messages' do
+    let(:test_commands) { Messages.new }
+    context 'Object creates Default Parameters' do
+      describe '#tech_joke' do
+        s = "\nQ. Why couldn't the developer pull the weeds from the garden?\nA. They didn't have root access."
+        it 'joke should parse to a String and sent to the BOT to display' do
+          expect(test_commands.tech_joke.class).to eq(s.class)
+        end
 
-describe Messages do
-  let(:test_commands) { Messages.new }
-  context 'Object creates Default Parameters' do
-    describe '#tech_joke' do
-      s = "\nQ. Why couldn't the developer pull the weeds from the garden?\nA. They didn't have root access."
-      it 'joke should parse to a String and sent to the BOT to display' do
-        expect(test_commands.tech_joke.class).to eq(s.class)
+        it 'joke should not parse to an Integer' do
+          expect(test_commands.tech_joke.class).not_to eq(Integer)
+        end
       end
 
-      it 'joke should not parse to an Integer' do
-        expect(test_commands.tech_joke.class).not_to eq(Integer)
-      end
-    end
+      describe '#yomomma_joke' do
+        s = 'Yo mama so fat, her stomach gets home 15 minutes before she does.'
+        it 'joke should parse to a String and sent to the BOT to display' do
+          expect(test_commands.yomomma_joke.class).to eq(s.class)
+        end
 
-    describe '#yomomma_joke' do
-      s = 'Yo mama so fat, her stomach gets home 15 minutes before she does.'
-      it 'joke should parse to a String and sent to the BOT to display' do
-        expect(test_commands.yomomma_joke.class).to eq(s.class)
+        it 'joke should not parse to an Integer' do
+          expect(test_commands.yomomma_joke.class).not_to eq(Integer)
+        end
       end
 
-      it 'joke should not parse to an Integer' do
-        expect(test_commands.yomomma_joke.class).not_to eq(Integer)
-      end
-    end
+      describe '#helper' do
+        it 'help_command parse to a Stringand sent to the BOT to display' do
+          expect(test_commands.helper.class).to eq(String)
+        end
 
-    describe '#helper' do
-      it 'help_command variable is not NULL' do
-        expect(test_commands.helper).not_to eq(nil)
+        it 'help_command parse should not parse to an Integer' do
+          expect(test_commands.helper.class).not_to eq(Integer)
+        end
+
+        it 'help_command variable is not NULL' do
+          expect(test_commands.helper).not_to eq(nil)
+        end
       end
     end
   end
